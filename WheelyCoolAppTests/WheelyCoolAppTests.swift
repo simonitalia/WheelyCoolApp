27d53adcc40ff75cba057c2b32cbae75c6ea1306
Simon Italia <sitalia@magicaltomato.com>
2020-07-02 11:20:53 +1000

    Initial Commit

---
+++ WheelyCoolAppTests/WheelyCoolAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  WheelyCoolAppTests.swift
+//  WheelyCoolAppTests
+//
+//  Created by Simon Italia on 7/2/20.
+//  Copyright © 2020 SDI Group Inc. All rights reserved.
+//
+
+import XCTest
+@testable import WheelyCoolApp
+
+class WheelyCoolAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
